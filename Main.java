059501d95b495e75b418030e6a38ea006943f8a8
karturbis <vulkanera42@gmx.ch>
2023-11-28 16:03:00 +0100

    initial commit

---
+++ Main.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/*
+ * Main class, contains the main method which is executed by running the program
+ */
+public class Main {
+    //Class variables:
+    ArrayList<String> commands = new ArrayList<String>();
+    ArrayList<Stuhl> a1sArrayList = new ArrayList<Stuhl>();
+    ArrayList<Tisch> a1tArrayList = new ArrayList<Tisch>();
+
+
+    /*
+     * The main method
+     */
+    public static void main(String[] args) {
+
+        Main main = new Main();
+
+        main.commands.add("help");
+        main.commands.add("quit");
+        main.commands.add("aufgabe1");
+        //TODO: add new commands to the commands arraylist.
+
+        main.clui();
+    }
+    
+    /*
+     * the command line user interface executes commands based on the user input.
+     */
+    public void clui(){
+
+        String command = "";
+        Scanner input = new Scanner(System.in);
+        help();
+
+        while (!command.equals("q")) {
+
+            System.out.print(">> ");
+            command = input.nextLine(); 
+
+            switch (command) {
+                case "?":
+                case "help":
+                case "h":
+                    help();
+                    break;
+
+                case "q":
+                case "exit":
+                case "quit":
+                    command = "q";
+                    break;
+                
+                case "a1":
+                case "aufgabe1":
+                    a1();
+                    break;
+            
+                case"command":
+                    //TODO: implement commands
+                    break;
+                    
+                case "":
+                    break;
+
+                default:
+                    System.out.println("The input could not be processed.");
+            }
+
+        }
+
+        input.close();
+    }
+
+    /*
+     * help method, out prints all available commands
+     */
+    public void help(){
+
+        System.out.println("List of available commands:");
+        for (String command : commands) {
+            System.out.println(command);
+            
+        }
+    }
+    /*
+     * method for task 1, creates all needed furniture and puts it into the right place
+     */
+    public void a1(){
+
+        for(int i = 0; i < 2; i++){
+
+            a1tArrayList.add(a1t + i);
+        }
+
+    }
+
+
+}
+
+
